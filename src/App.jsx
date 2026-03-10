43a36528befa07ff3a2066b50b2c5ba66a1f0acb
letxiz <letxizalves@gmail.com>
2026-03-10 17:26:26 -0400

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Footer from "./components/Footer";
+function App(){
+  return (
+    <div>
+      <h1>
+        My Daily Habits
+      </h1>
+      <p>Gerencie seus hábitos diários de forma simples e visual</p>
+
+      <Footer/>
+    </div>
+  );
+}
+
+export default App
