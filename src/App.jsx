41f2435268ff8cd086a7587551f6d62abfd55d19
letxiz <letxizalves@gmail.com>
2026-03-16 16:29:56 -0400

    feat: ajustando erro no import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react"
 import './App.css'
 import Header from './components/Header'
 import Footer from "./components/Footer"
-import BemVindo from './components/Bemvindo'
+import BemVindo from './components/BemVindo'
 import SecaoHabitos from './components/SecaoHabitos'
 import HabitList from './components/HabitList'
 
